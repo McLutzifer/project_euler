a37c59a54d0f8cbb889b3415cd98db14944e8c00
McLutzifer <lukasschumi@gmx.at>
2021-01-31 22:21:56 +0100

    add actal code

---
+++ Problem_1.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace euler 
+{
+    class Problem1 
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            for (int i=1; i<1000; i++)
+            {
+                if (((i%3) == 0) || (i%5) == 0)
+                {
+                    result += i;
+                }
+            }
+
+            Console.WriteLine("The sum on numbers div. by 3 and 5 is {0}", result);
+        }       
+    }
+}   
